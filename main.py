3d8be3270cbf481f68ab46161600ff3a1ad9ea14
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-08-13 18:02:21 +0900

    1.4 Global Time Series Part Two

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+
+def make_global_df(condition):
+    df = pd.read_csv(f"data/time_{condition}.csv")
+    df = (
+        df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
+        .sum()
+        .reset_index(name=condition)
+    )
+    df = df.rename(columns={"index": "date"})
+    return df
+
+
 daily_df = pd.read_csv("data/daily_report.csv")
 
 totals_df = (
@@ -9,3 +21,15 @@ totals_df = totals_df.rename(columns={"index": "condition"})
 
 countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
 countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+
+conditions = ["confirmed", "deaths", "recovered"]
+
+
+final_df = None
+
+for condition in conditions:
+    condition_df = make_global_df(condition)
+    if final_df is None:
+        final_df = condition_df
+    else:
+        final_df = final_df.merge(condition_df)
