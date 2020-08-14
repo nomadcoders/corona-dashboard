0b140cd8e82931f7ea292facb6273b8d29b32798
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-08-14 17:16:07 +0900

    2.2 Countries Table part One

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+from data import countries_df
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -21,9 +22,43 @@ app.layout = html.Div(
         html.Header(
             style={"textAlign": "center", "paddingTop": "50px"},
             children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
-        )
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Table(
+                            children=[
+                                html.Thead(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Th(column_name.replace("_", " "))
+                                                for column_name in countries_df.columns
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                html.Tbody(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Td(value_column)
+                                                for value_column in value
+                                            ]
+                                        )
+                                        for value in countries_df.values
+                                    ]
+                                ),
+                            ]
+                        )
+                    ]
+                )
+            ]
+        ),
     ],
 )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
